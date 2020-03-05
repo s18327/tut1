0f26a90ed846a6fc2e456fb14598bfa7b5daea63
s18327 <s18327@pjwstk.edu.pl>
2020-03-05 16:54:54 +0100

    first commit (on tutorials)

---
+++ Cw1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+          
+
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(args[0]);
+
+            if (response.IsSuccessStatusCode) {
+                var content =await response.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",RegexOptions.IgnoreCase);
+                var matches = regex.Matches(content);
+
+                foreach (var email in matches) {
+                    Console.WriteLine(email.ToString());
+                }
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
